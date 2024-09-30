f4a15f94d2e9e1b7781b416f485c38f2fdd45cab
Raghad-Suwan <116986130+Raghad-Suwan@users.noreply.github.com>
2024-09-30 18:15:25 +0300

    Time: 62 ms (32.71%) | Memory: 50.1 MB (77.25%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -4,29 +4,21 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    let n = candies.length;
-    let result = new Array(n);
+    let maxCandies = candies[0];
+    let result=new Array(candies.length)
 
-    let maxCandies=candies[0];
-    for (let i = 1; i < n; i++){
-        if (candies[i]>maxCandies  )
-        {
-            maxCandies=candies[i];
-        }
-
+    for ( let i =1 ; i< candies.length ; i++){
+        if (candies[i]>maxCandies )
+        maxCandies =candies[i];
     }
-
-
-    for (let i = 0; i < n; i++) {
-        if (candies[i] + extraCandies >= maxCandies) {
-            result[i] = true;
-        } else {
-            result[i] = false;
-        }
+    for ( let i =0 ; i< candies.length ; i++){
+        if( candies[i] + extraCandies >= maxCandies){
+        result[i] = true 
     }
+    else result [i]= false 
+    }
+    return result
 
-
-        return result;
 
 
 };
