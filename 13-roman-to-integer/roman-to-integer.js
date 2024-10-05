373070d1eb9138b0e1134239a277c48d5d294793
Raghad-Suwan <116986130+Raghad-Suwan@users.noreply.github.com>
2024-10-05 20:41:42 +0300

    Time: 141 ms (8.47%) | Memory: 54.8 MB (45.99%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,35 +3,34 @@
  * @return {number}
  */
 
- function romanValue(roman){
-  if (roman === 'I') return 1;
-  if (roman === 'V') return 5;
-  if (roman === 'X') return 10;
-  if (roman === 'L') return 50;
-  if (roman === 'C') return 100;
-  if (roman === 'D') return 500;
-  if (roman === 'M') return 1000;
-  return -1;
- }
 var romanToInt = function(s) {
+    const romanValue ={
+        'I' : 1 , 
+        'V' : 5,
+        'X' : 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+
+    }
     let result =0;
 
     for ( let i=0 ; i< s.length ; i++){
-
-        let num1=romanValue(s[i]);
+        let num1 = romanValue[s[i]];
         if (i + 1 < s.length){
-            let num2 = romanValue(s[i + 1]);
+            let num2 = romanValue[s[i + 1]];
             if (num1 >= num2) {
                 result += num1;
                 } else {
                     result += (num2 - num1);
                     i++;
-                    }
+                }
 
-        } else {
-            result += num1;
-            }       
+        }else{
+        result += num1;
+        }       
     }
-      return result;
+    return result;
 
 };
